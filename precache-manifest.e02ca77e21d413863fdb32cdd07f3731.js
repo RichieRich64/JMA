861cec9218ac33f3bdc7bdd3b0c9327f34c564dd
Richard RichieRich <rico4live@gmail.com>
2020-07-20 11:52:15 -0700

    Updates

---
+++ precache-manifest.e02ca77e21d413863fdb32cdd07f3731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f831df916f2358c81e8907d2ed1fb7d",
+    "url": "/JMA/index.html"
+  },
+  {
+    "revision": "194b04aeb57782aa4f52",
+    "url": "/JMA/static/css/main.43b8a441.chunk.css"
+  },
+  {
+    "revision": "32e8d34910551b44918d",
+    "url": "/JMA/static/js/2.5d9b99d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JMA/static/js/2.5d9b99d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "194b04aeb57782aa4f52",
+    "url": "/JMA/static/js/main.b0011ca2.chunk.js"
+  },
+  {
+    "revision": "6cad70344ab7b77ecb01",
+    "url": "/JMA/static/js/runtime-main.1206ccb2.js"
+  }
+]);
